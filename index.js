be4fe160606147ffb99929b71df66e12ae7e0a25
louis <louis.lamonte@moringaschool.com>
2024-03-25 12:23:42 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Write your solution here!
+let cats =[ "Milo","Otis","Garfield"]
+
+before(function(){
+    cats.length = 0
+    cats.push("Milo","Otis","Garfield")
+})
+
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+destructivelyAppendCat(Ralph)
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+destructivelyRemoveLastCat()
+
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+destructivelyPrependCat(Bob)
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+destructivelyRemoveFirstCat()
+
+function appendCat(){
+    let newCats = [... cats,"Broom"]
+    return newCats
+}
+
+function prependCat(){
+    let newCats = ["Arnold",... cats]
+    return newCats
+}
+
+function removeLastCat(){
+    return cats.slice(0,-1)
+
+}
+
+function removeFirstCat(){
+    return cats.slice(1)
+}
+
+
